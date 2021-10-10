9dffdc4276de4b00bbe5a70cac7972fa1fb642b1
Sunity Khan <73030786+Sunity1481@users.noreply.github.com>
2021-10-10 14:02:11 +0600

    Add files via upload

---
+++ LAB 5.2 [PHP] [SESSION TRACKING]/PUBLIC_HOME.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Public Home Page</title>
+</head>
+<body>
+	<table border="1" width="100%">
+		<tr height="50px">
+			<td width="100px">
+				<img src="logo.png" width="400px" height="200px">
+			</td>
+	             
+             <td colspan="2" align="right"> 
+	             <a href="A_Public_Home.php">Home</a>
+	             <a href="C_Login.php">Login</a>
+	             <a href="B_Registration.php">Registration</a>
+             </td>
+         </tr>
+         <tr height="100px">
+         	<td colspan="2" align="center">
+         		 <h1>Welcome to xCompany</h1>
+         	</td>
+         </tr>
+         <tr>
+         	<td colspan="2" align="center">
+         		 <p>Copyright@2017</p>
+         	</td>
+         </tr>     
+    </table>
+</body>
+</html>
